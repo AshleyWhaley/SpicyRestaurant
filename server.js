db6dc00e725f33154f31d84cd5868f1f925d185b
zackjj <zack.j.johnston@gmail.com>
2021-03-04 19:02:40 -0800

    added blank files

---
+++ server.js
@@ -0,0 +1,8 @@
+//dependencies
+const express = require('express')
+//express server
+const app = express()
+const PORT = 5000
+//response handler
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`)); 
